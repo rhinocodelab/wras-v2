0f3b14bc1c71e29b81884ecd598214ccf8d12b01
Prashant Pokhriyal <rhinocodelab@gmail.com>
2025-07-27 10:00:41 +0000

    still it like that only. Rebuild the sample dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -75,9 +75,9 @@ export default async function HomePage() {
             </SidebarMenu>
           </SidebarContent>
         </Sidebar>
-        <div className="flex flex-col sm:pl-14">
+        <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
           <Header session={session} />
-          <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-4 md:gap-8">
+          <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
             <div className="flex items-center">
               <h1 className="text-lg font-semibold md:text-2xl">
                 Train Search
